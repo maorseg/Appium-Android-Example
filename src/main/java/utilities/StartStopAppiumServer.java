6c7e64f2df4db7d939b0649b90b27790b30fe68b
Maor Segev <37215368+maorseg@users.noreply.github.com>
2018-05-10 18:55:55 +0300

    Update StartStopAppiumServer.java

--- src/main/java/utilities/StartStopAppiumServer.java
+++ src/main/java/utilities/StartStopAppiumServer.java
@@ -7,8 +7,6 @@ import io.appium.java_client.service.local.AppiumDriverLocalService;
 import io.appium.java_client.service.local.AppiumServiceBuilder;
 import io.appium.java_client.service.local.flags.GeneralServerFlag;
 
-
-
 public class StartStopAppiumServer {
 
     private AppiumDriverLocalService appiumService;
