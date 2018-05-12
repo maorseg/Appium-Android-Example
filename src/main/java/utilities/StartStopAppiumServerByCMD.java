45533ec7f085a62b023e11651b76b2007451b00d
Maor Segev <37215368+maorseg@users.noreply.github.com>
2018-05-12 03:43:24 +0300

    Update StartStopAppiumServerByCMD.java

--- src/main/java/utilities/StartStopAppiumServerByCMD.java
+++ src/main/java/utilities/StartStopAppiumServerByCMD.java
@@ -1,7 +1,5 @@
 package utilities;
 
-import io.appium.java_client.service.local.AppiumServiceBuilder;
-import io.appium.java_client.service.local.flags.GeneralServerFlag;
 import org.testng.annotations.Test;
 import java.io.IOException;
 
